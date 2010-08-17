3e85467b3acc771811c92a19225225ba41dc2a01
Nick Joyce <nick@boxdesign.co.uk>
2010-08-17 11:37:36 +0800

    Update AMFController to be compatible with PyAMF 0.4->0.6

--- web/extras/rpc/amf/__init__.py
+++ web/extras/rpc/amf/__init__.py
@@ -24,17 +24,15 @@ except ImportError:
 
 
 class AMFController(Dialect):
-    __context__ = pyamf.AMF0
     __gateway__ = dict()
-    
+
     def __init__(self):
         self._gateway = pyamf.remoting.gateway.BaseGateway(logger=log, *self.__gateway__)
-        self._context = pyamf.get_context(self.__context__)
-    
+
     def __call__(self, request):
-        pyamf_request = pyamf.remoting.decode(request.body, self._context)
-        pyamf_response = pyamf.remoting.Envelope(pyamf_request.amfVersion, pyamf_request.clientType)
-        
+        pyamf_request = pyamf.remoting.decode(request.body)
+        pyamf_response = pyamf.remoting.Envelope(pyamf_request.amfVersion)
+
         for name, message in pyamf_request:
             # Dynamically build mapping.  This introduces a performance hit on the first request of each method.
             if message.target not in self._gateway.services:
@@ -44,4 +42,5 @@ class AMFController(Dialect):
             pyamf_response[name] = self._gateway.getProcessor(message)(message)
         
         response.headers['Content-Type'] = pyamf.remoting.CONTENT_TYPE
-        return pyamf.remoting.encode(pyamf_response, self._context).getvalue()
+
+        return pyamf.remoting.encode(pyamf_response).getvalue()
